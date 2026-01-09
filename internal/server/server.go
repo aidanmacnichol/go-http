59832a065effceffcbe22b03e3f286b0821eca7c
Aidan MacNichol <aidanmacnichol11@gmail.com>
2026-01-08 17:56:33 -0700

    feature: basic http-server working

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,80 +3,63 @@ package server
 import (
 	"fmt"
 	"go-http/internal/request"
+	"log"
 	"net"
-	"strconv"
-	"sync"
 	"sync/atomic"
 )
 
-// Contains the state of the server
 type Server struct {
-	Listener    net.Listener
-	Closed      uint32 // Bool flag - 32 bit because many CPU architectures only support world aligned boundaries
-	mu          sync.Mutex
-	ActiveConns map[net.Conn]bool
+	Listener net.Listener
+	Closed   atomic.Bool
 }
 
-// Creates a net.Listener and returns a new Server instance. Starts listening for requests inside a goroutine.
 func Serve(port int) (*Server, error) {
-	portAddr := ":" + strconv.Itoa(port)
-	listener, err := net.Listen("tcp", portAddr)
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		return nil, err
 	}
-	s := Server{
+	s := &Server{
 		Listener: listener,
-		Closed:   0,
 	}
 	go s.listen()
-	return &s, nil
+	return s, nil
 }
 
-// Closes the listener and the server
 func (s *Server) Close() error {
-	atomic.StoreUint32(&s.Closed, 1)
-	fmt.Println("Closing server...")
-	s.Listener.Close()
-
-	// What to do with errors????
+	s.Closed.Store(true)
+	if s.Listener != nil {
+		return s.Listener.Close()
+	}
 	return nil
 }
 
-// Uses a loop to .Accept new connections as they come in,
-// and handles each one in a new goroutine. I used an atomic.
-// Bool to track whether the server is closed or not so that I can ignore connection errors after the server is closed.
 func (s *Server) listen() {
 	for {
 		conn, err := s.Listener.Accept()
 		if err != nil {
-			fmt.Println("Accept Error: ", err)
+			if s.Closed.Load() {
+				return
+			}
+			log.Printf("Accept Error: %v", err)
 			continue
 		}
-
-		s.mu.Lock()
-		s.ActiveConns[conn] = true
-		s.mu.Unlock()
-
 		go s.handle(conn)
 	}
 }
 
-// Handles a single connection by writing the following response and then closing the connection:
 func (s *Server) handle(conn net.Conn) {
-	defer func() {
-		s.mu.Lock()
-		delete(s.ActiveConns, conn)
-		s.mu.Unlock()
-		conn.Close()
-	}()
+	defer conn.Close()
 
 	_, err := request.RequestFromReader(conn)
 	if err != nil {
 		fmt.Printf("Error parsing request: %v\n", err.Error())
 		return
 	}
+	response := "HTTP/1.1 200 OK\r\n" +
+		"Content-Type: text/plain\r\n" +
+		"Content-Length: 13\r\n" +
+		"\r\n" +
+		"Hello World!\n"
 
-	fmt.Println("HTTP/1.1 OK")
-	fmt.Println("Content-Type: text/plain")
-	fmt.Println("\nHello World!")
+	conn.Write([]byte(response))
 }
